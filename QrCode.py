ce9fc56ecb07490d0ba62159c02d1380f7dde0b5
Gabriel <morenobiel2008@gmail.com>
2026-01-30 09:46:12 -0300

    Finalizado oficial

--- QrCode.py
+++
@@ -1,9 +0,0 @@
-# from PyQt5.uic import loadUi
-# from PyQt5.QtWidgets import QMainWindow
-
-# class QrCodeUI(QMainWindow): 
-
-#     def __init__(self, **kwargs):
-#         super().__init__(**kwargs)
-#         loadUi(("QrCodeBuilder.ui"), self)
-#         self.show()
